4f03ceaf186f3e09f86da169a9ffe7fde309551d
Macro-internshala <macro-internshala@test.com>
2024-08-11 16:13:59 +0530

    added current weather

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ module.exports = {
     extend: {
       fontFamily: {
         'Nunito': ["Nunito Sans", 'sans-serif'],
-      }
+      },
+      backgroundColor: {
+        'systemGray': '#8E8E93',
+        'systemGray2': '#636366',
+        'systemGray3': '#48484A',
+        'systemGray4': '#3A3A3C',
+        'systemGray6': '#1C1C1E',
+      },
+      textColor: {
+        'systemGray': '#8E8E93',
+        'systemGray2': '#636366',
+        'systemGray3': '#48484A',
+        'systemGray4': '#3A3A3C',
+        'systemGray6': '#1C1C1E',
+      },
     },
   },
   plugins: [],
